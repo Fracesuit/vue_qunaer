4069fea6718058d70086619c605280860296bd01
zhiren.zhang <915924056@qq.com>
2018-07-26 17:35:50 +0800

    111

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="header-left"><span class="iconfont icon-back">&#xe624;</span></div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span>输入框</div>
+    <div class="header-right">城市<span class="iconfont icon-arrow">&#xe64a;</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  /*1 rem=50px  2倍图*/
+  /*@import "~@/assets/styles/varibles.styl"  引入别名*/
+  @import "~styles/varibles.styl"
+  .header
+    display flex
+    display -webkit-flex
+    line-height: .86rem
+    background: $bgcolor
+    color :#ffffff
+    .header-left
+      width: .64rem
+      float: left
+      text-align center
+      .icon-back
+          font-size .20rem
+    .header-input
+      flex: 1
+      background: #ffffff
+      height .62rem
+      line-height .62rem
+      margin-top .12rem
+      padding-left .2rem
+      margin-left .2rem
+      border-radius .1rem
+      color #ccc
+      text-align left
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align center
+      .icon-arrow
+        font-size .20rem
+        text-align center
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperSlides" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperSlides: [
+        {
+          id: '1',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/4f/37f83b87138a0c02.jpg_750x200_8e5df442.jpg'
+        },
+        {
+          id: '2',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/4f/37f83b87138a0c02.jpg_750x200_8e5df442.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  /*处理图片抖动问题，提前占位
+  height 26.7%vw  有兼容性问题*/
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+
+  .wrapper
+    overflow hidden
+    background #eee
+    width 100%
+    height: 0
+    padding-bottom 26.7%
+    .swiper-img
+      width 100%
+</style>
