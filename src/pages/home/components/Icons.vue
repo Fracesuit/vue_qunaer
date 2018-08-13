82b82f019437e5b911ca0e3156da55f66753265d
zhiren.zhang <915924056@qq.com>
2018-08-13 18:11:12 +0800

    123

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,28 +1,97 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages1" :key="index">
+        <div class="icon" v-for="item of page" v-bind:key="item.id">
+          <div class="icon-img">
+            <!-- <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>-->
+            <img :src="item.imgUrl" class="icon-img-content">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票1'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票2'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票3'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票5'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票6'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票7'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票8'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票9'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages1 () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        let page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .icons
-    overflow: hidden
-    height: 0
-    width: 100%
-    padding-bottom 50%
+    .swiper-container
+      height: 0
+      width: 100%
+      padding-bottom 50%
     .icon
       position relative
       overflow hidden
